2a62b5106c6f781a76bc53137b226e9887a72931
nhawshow <andrenfranca1@gmail.com>
2025-08-29 14:26:34 -0300

    .

--- guessing_game/src/game.rs
+++ guessing_game/src/game.rs
@@ -5,29 +5,34 @@ use rand::Rng; // biblioteca para gerar numeros aleatorios
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess, NOW:");
-
     let secret_number = rand::thread_rng().gen_range(1..=100); // de 1 até 100, incluindo o 100
 
     println!("Secret number is: {secret_number}");
 
-    let mut guess = String::new();
+    loop {
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line"); // se der erro, printa a msg
+        println!("Please input your guess.");
 
-    let guess: u32 = guess
-    .trim() // remove espaços em branco
-    .parse() // converte string para numero (unsigned 32 bits)
-    .expect("Please type a number!"); // se der erro, printa a msg
+        let mut guess = String::new();
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); // tratamento de erro
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        }; // trim remove espaços em branco, parse converte string para numero
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
-
 }
