81fee2fb629190ff76452c0b394be3254cbc01aa
AndrewNha <andrenfranca1@gmail.com>
2025-08-29 11:25:01 -0300

    Dificil ASF

--- guessing_game/src/game.rs
+++ guessing_game/src/game.rs
@@ -1,6 +1,30 @@
+use std::io;
+use std::cmp::Ordering; // serio que tem q usar biblioteca pra porra do if.
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hytalo Santos Inocente");
-    println!("Felca Racista..");
-    println!("Agr o bgl ta serio, aq é LINUX");
+    println!("Guess the number!");
+
+    println!("Please input your guess, NOW:");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Secret number is: {secret_number}");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
 }
